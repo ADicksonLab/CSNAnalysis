e2b82bc30a15efbed06e728873fbac1173096bf2
Alex Dickson <alexrd@msu.edu>
2018-03-02 10:25:13 -0500

    install info

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CSNAnalysis',
+    version='0.1.0-beta',
+    py_modules=['csnanalysis'],
+    author='Alex Dickson',
+    author_email='alexrd@msu.edu',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Numpy',
+        'NetworkX',
+        'Scipy',
+    ],
+)
